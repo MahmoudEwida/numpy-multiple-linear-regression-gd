68fc51287ca882016883aeaef79debcddfe2c30d
MahmoudEwida <ewidaxq@gmail.com>
2026-09-17 21:17:33 +0300

    Step 3: compute_feature_stats

--- model.py
+++ model.py
@@ -42,8 +42,15 @@ def split_train_val_test(X, y, train_frac=0.6, val_frac=0.2):
         X[val_end:], y[val_end:]
     )
 
-# Step 3 - compute_feature_stats (not yet solved)
-# TODO: implement
+# Step 3 - compute_feature_stats
+import test
+def compute_feature_stats(X):
+    # TODO: Compute per-feature mean and std; replace std of 0 with 1
+    mean = np.mean(X, axis = 0)
+    std = np.std(X, axis = 0)
+
+    std[std == 0] = 1.0
+    return mean,std
 
 # Step 4 - standardize_features (not yet solved)
 # TODO: implement
