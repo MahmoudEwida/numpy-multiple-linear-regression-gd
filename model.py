3d8a0153235178f82027c5ef85d7e7d67b065dd4
MahmoudEwida <ewidaxq@gmail.com>
2026-09-18 20:35:26 +0300

    Step 28: compare_with_normal_equation

--- model.py
+++ model.py
@@ -361,6 +361,8 @@ def score_lr_model(model, X, y):
 
     return metrics
 
-# Step 28 - compare_with_normal_equation (not yet solved)
-# TODO: implement
+# Step 28 - compare_with_normal_equation
+def compare_with_normal_equation(model):
+    # TODO: Return the L2 distance between GD and normal-equation weights.
+    return weights_l2_distance(model["weights"], model["normal_weights"])
 
