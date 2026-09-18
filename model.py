53d2e4b03eaafd7c6a899c785be9884f1612f948
MahmoudEwida <ewidaxq@gmail.com>
2026-09-18 20:30:56 +0300

    Step 26: predict_lr_model

--- model.py
+++ model.py
@@ -341,8 +341,12 @@ def fit_lr_model(model, X_train, y_train, X_val, y_val):
 
     return model
 
-# Step 26 - predict_lr_model (not yet solved)
-# TODO: implement
+# Step 26 - predict_lr_model
+def predict_lr_model(model, X):
+    # TODO: Return predicted targets for raw X using the fitted model.
+    X_tr_design = prepare_design_matrix(X, model['mean'], model['std'])
+
+    return predict_linear(X_tr_design,model['weights'])
 
 # Step 27 - score_lr_model (not yet solved)
 # TODO: implement
