1d4b663c34ffad3e021abb735ee04d64d5b6cf48
MahmoudEwida <ewidaxq@gmail.com>
2026-09-18 15:49:10 +0300

    Step 11: initialize_weights

--- model.py
+++ model.py
@@ -101,8 +101,14 @@ def normal_equation(X, y):
 
     return np.linalg.pinv(X.T @ X) @ X.T @ y
 
-# Step 11 - initialize_weights (not yet solved)
-# TODO: implement
+# Step 11 - initialize_weights
+import numpy as np
+def initialize_weights(n_features, seed=None):
+    # TODO: Return (n_features,) weights sampled from N(0, 0.01)
+    if seed is not None : 
+        np.random.seed(seed)
+
+    return np.random.normal(0,0.01,size = n_features)
 
 # Step 12 - gd_step (not yet solved)
 # TODO: implement
